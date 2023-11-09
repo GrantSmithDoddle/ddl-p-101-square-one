c32f2b75850048da0da1e76967f65bb29adfe277
GrantSmithDoddle <grant@doddle.agency>
2023-11-09 17:25:16 +0000

    Moved doddle base into project

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,11 @@ import sitemap from '@astrojs/sitemap';
 import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
-import mdx from '@astrojs/mdx';
-
-// https://astro.build/config
 export default defineConfig({
   output: 'static',
   site: 'https://squareonerenovations.co.uk',
   sitemap: true,
-  integrations: [sitemap(), robotsTxt(), mdx()],
+  integrations: [sitemap(), robotsTxt()],
   output: 'static',
-  trailingSlash: 'ignore'
+  trailingSlash: 'ignore',
 });
